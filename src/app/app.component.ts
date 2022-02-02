e2cfdbc268a6365e8848b73b509d7fc10a13c1f1
erockrazor <eric.gieg@proplogix.com>
2022-02-01 20:09:48 -0500

    feat: adding csv extractor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'randomTools';
+export class AppComponent implements OnInit {
+  public unformattedText: string = ''
+  public formattedText: string = ''
+  public header: string[] = []
+  public data: string[] = []
+  public exampleText = `Date:
+  04/08/2022
+  Name:
+  Eric`
+
+  ngOnInit(): void {
+    this.buildCSV(this.unformattedText)
+  }
+
+  public buildCSV = (unformattedText: string) => {
+    this.formattedText = unformattedText
+    this.header = []
+    this.data = []
+    const rows = unformattedText.split('\n')
+    rows.forEach((row, i) => {
+      if (i % 2 === 0) {
+        this.header.push(row.trim().replace(':', ''))
+      } else {
+        this.data.push(row.trim().replace('\t', ''))
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AppComponent } from './app.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
